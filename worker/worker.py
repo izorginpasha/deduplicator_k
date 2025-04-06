dfc28e58b604a4f65b07232fcb2cd0a7c516f238
izorgin.pasha <izorgin.pasha@yandex.ru>
2025-04-06 17:39:26 +0500

    воркер не выдержал нагрузки 200 rps

--- worker/worker.py
+++ worker/worker.py
@@ -8,12 +8,14 @@ QUEUE_KEY = "events:queue"
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 dedup = Deduplicator(redis=redis_client)
 
+
 async def process(event: dict):
     print(f"✅ Обрабатываем уникальное событие: ")
     # Здесь может быть логика сохранения в БД, логирования и т.п.
 
-async def worker_loop():
-    print("🚀 Воркер запущен и ожидает событий...")
+
+async def worker_loop(worker_id: int):
+    print(f"🚀 Воркер {worker_id} запущен и ожидает событий...")
     while True:
         result = await redis_client.blpop(QUEUE_KEY, timeout=1)
         if result:
@@ -23,9 +25,21 @@ async def worker_loop():
             if await dedup.is_unique(event):
                 await process(event)
             else:
-                print(f"⛔ Дубликат события: ")
+                print(f"⛔ Дубликат события от воркера {worker_id}")
 
         await asyncio.sleep(0.01)
 
+
+async def start_workers(worker_count: int):
+    tasks = []
+    for i in range(worker_count):
+        # Запускаем несколько воркеров с уникальными ID
+        tasks.append(asyncio.create_task(worker_loop(i + 1)))
+
+    # Запускаем все воркеры параллельно
+    await asyncio.gather(*tasks)
+
+
 if __name__ == "__main__":
-    asyncio.run(worker_loop())
+    # Запускаем 5 воркеров
+    asyncio.run(start_workers(5))
